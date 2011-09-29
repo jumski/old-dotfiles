99a09a5a6cb313759bb8ec83d6c03c4b40349e56
Wojtek Majewski <jumski@gmail.com>
2011-09-29 17:54:22 +0200

    fixed PATH

--- bashrc.sh
+++ bashrc.sh
@@ -8,7 +8,7 @@ export DOTFILES_PATH=~/dotfiles/
 source $DOTFILES_PATH/bash/aliases.sh
 . $DOTFILES_PATH/bash/colors.sh
 
-export PATH=$HOME/local/node/bin:$DOTFILES_PATH/bin:/home/jumski/various/bin:$PATH
+export PATH=$HOME/local/node/bin/:$DOTFILES_PATH/bin/:/home/jumski/various/bin/:$PATH
  
 ## LOAD RVM
 [[ -s "$HOME/.rvm/scripts/rvm" ]] && . "$HOME/.rvm/scripts/rvm"
