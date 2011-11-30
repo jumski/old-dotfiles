dcd581d818666d1116e5f3177a23bc6e70af1592
Wojtek Majewski <jumski@gmail.com>
2011-11-30 13:21:50 +0100

    working tmux-blubase script

--- bin/tmux-blubase.sh
+++ bin/tmux-blubase.sh
@@ -1,17 +1,23 @@
 #!/bin/bash
 
-export WORKING_DIR=~/blubase-files
-cd $WORKING_DIR;
+export BLUBASE_DIR=~/work/blu/base
+cd $BLUBASE_DIR;
 
 tmux start-server
-tmux new-session -d -s blubase -n work
 
-tmux new-window -tblubase:1 -n svnbluair
-# tmux new-window -tblubase:2 -n test
+tmux new-session -d -s blubase
+tmux source-file ~/tmux.conf
 
-tmux send-keys -tblubase:0 'cd $WORKING_DIR' C-m
-tmux send-keys -tblubase:1 'ssh svnbluair' C-m
-# tmux send-keys -tblubase:2 'cd $WORKING_DIR && bundle exec autotest -f' C-m
+tmux new-window -tblubase -n server
+tmux new-window -tblubase -n console
+tmux new-window -tblubase -n vim
+tmux new-window -tblubase -n bash
+tmux kill-window -t 0
 
-tmux select-window -tblubase:0
+tmux send-keys -tblubase:1 "script/server" C-m
+tmux send-keys -tblubase:2 "script/console" C-m
+tmux send-keys -tblubase:3 "vim -c Gstatus" C-m
+tmux send-keys -tblubase:4 "git wtf" C-m
+
+tmux select-window -tblubase:4
 tmux attach-session -d -tblubase
