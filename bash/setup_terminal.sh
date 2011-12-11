6f88e2bd2a9f318c8c38d71096315f1de0833ad6
Wojtek Majewski <jumski@gmail.com>
2011-12-11 01:42:13 +0100

    fix for bundler-exec setup

--- bash/setup_terminal.sh
+++ bash/setup_terminal.sh
@@ -25,4 +25,4 @@ if [ -f /etc/bash_completion ] && ! shopt -oq posix; then
     . /etc/bash_completion
 fi
 
-[ -f ~/.bundler-exec.sh ] && source ~/.bundler-exec.sh
+[ -f $DOTFILES_PATH/bash/bundler-exec.sh ] && source $DOTFILES_PATH/bash/bundler-exec.sh
