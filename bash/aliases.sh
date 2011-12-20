60761470a335b264c98db955ee1e62a9fc896d72
Wojtek Majewski <jumski@gmail.com>
2011-12-20 17:24:49 +0100

    add mkdir -p alias

--- bash/aliases.sh
+++ bash/aliases.sh
@@ -15,6 +15,7 @@ alias ll='ls -alF'
 alias la='ls -A'
 alias l='ls -CF'
 alias alert='notify-send --urgency=low -i "$([ $? = 0 ] && echo terminal || echo error)" "$(history|tail -n1|sed -e '\''s/^\s*[0-9]\+\s*//;s/[;&|]\s*alert$//'\'')"'
+alias mkdir='mkdir -p'
 
 ## CUSTOM ALIASES
 alias ack=ack-grep
