1ba52fddd511612afab9674ca837c3be5d58d58d
Wojtek Majewski <jumski@gmail.com>
2011-12-21 16:04:14 +0100

    more stuff deleted

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#/bin/bash
 
 echo 'Installing scripts'
 export DOTFILES_PATH=~/dotfiles
@@ -18,14 +18,6 @@ ln -s "$DOTFILES_PATH/vim/vimrc" "$HOME/.vimrc"
 rm -f "$HOME/.gemrc"
 ln -s "$DOTFILES_PATH/gemrc" "$HOME/.gemrc"
 
-rm -f "$HOME/.Xdefaults"
-ln -s "$DOTFILES_PATH/vendor/Xresources" "$HOME/.Xdefaults"
-
-rm -f "$HOME/.dircolors"
-ln -s "$DOTFILES_PATH/vendor/dircolors.256dark" "$HOME/.dircolors"
-
 echo 'Load bashrc'
 source ~/dotfiles/bashrc.sh
 source ~/dotfiles/bash/aliases.sh
-
-confirm 'Installation complete. Do you want to customize?' && $DOTFILES_PATH/customize.sh
