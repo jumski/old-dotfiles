6b22eb8fd38a666b7c3c7bd6d0f1b02abac75248
Wojtek Majewski <jumski@gmail.com>
2011-12-21 19:35:32 +0100

    fix PS1 to autoresolve hostname

--- bash/colors.sh
+++ bash/colors.sh
@@ -7,4 +7,4 @@ c_violet="\[\e[34m\]"
 c_red="\[\e[31m\]"
 c_reset="\[\e[0m\]"
 
-PROMPT_COMMAND='export PS1="$c_celadon \u@bodhi $c_white`basename "$PWD"`$c_red \$ $c_reset"'
+PROMPT_COMMAND='export PS1="$c_violet \u@\H $c_white`basename "$PWD"`$c_red \$ $c_reset"'
