483c347e95b8ceb6be79bed9990876967df538c6
Wojtek Majewski <jumski@gmail.com>
2011-12-21 16:03:06 +0100

    change prompt

--- bash/colors.sh
+++ bash/colors.sh
@@ -7,14 +7,4 @@ c_violet="\[\e[34m\]"
 c_red="\[\e[31m\]"
 c_reset="\[\e[0m\]"
 
-function parse_git_dirty {
-  [[ $(git status 2> /dev/null | tail -n1) != "nothing to commit (working directory clean)" ]] && echo "*"
-}
-function parse_git_branch {
-  git branch --no-color 2> /dev/null | sed -e '/^[^*]/d' -e "s/* \(.*\)/\1/"
-}
-function parse_rvm_prompt {
-  rvm-prompt | sed 's/\(ruby-\)\(.*\)@\(.*\)/\3/'
-}
-
-PROMPT_COMMAND='export PS1=" $c_white`basename "$PWD"`$c_yellow [$(parse_git_branch)]$c_red $ $c_reset"'
+PROMPT_COMMAND='export PS1="$c_red \u@bodhi $c_white`basename "$PWD"` $c_red \$ $c_reset"'
