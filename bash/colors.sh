9d11e5a9e1e3e7526ef552ddf49e5364f448b325
Wojtek Majewski <jumski@gmail.com>
2011-12-21 18:44:07 +0300

    change prompt color

--- bash/colors.sh
+++ bash/colors.sh
@@ -7,4 +7,4 @@ c_violet="\[\e[34m\]"
 c_red="\[\e[31m\]"
 c_reset="\[\e[0m\]"
 
-PROMPT_COMMAND='export PS1="$c_red \u@bodhi $c_white`basename "$PWD"`$c_red \$ $c_reset"'
+PROMPT_COMMAND='export PS1="$c_celadon \u@bodhi $c_white`basename "$PWD"`$c_red \$ $c_reset"'
