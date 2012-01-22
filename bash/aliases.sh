b5529b0fd240f27dbe7650293d9b20069ee21663
Wojtek Majewski <jumski@gmail.com>
2012-01-22 22:06:19 +0100

    bash/aliases: add coltree alias for colored tree

--- bash/aliases.sh
+++ bash/aliases.sh
@@ -17,6 +17,10 @@ alias l='ls -CF'
 alias alert='notify-send --urgency=low -i "$([ $? = 0 ] && echo terminal || echo error)" "$(history|tail -n1|sed -e '\''s/^\s*[0-9]\+\s*//;s/[;&|]\s*alert$//'\'')"'
 alias mkdir='mkdir -p'
 
+coltree(){
+  tree -C "$@" | less -R
+}
+
 ## CUSTOM ALIASES
 alias ack=ack-grep
 
