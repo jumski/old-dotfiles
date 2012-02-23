e2d4c4f0267e5b9e5a2f4126b85b4141f89aee13
Wojtek Majewski <jumski@gmail.com>
2012-02-23 23:02:33 +0100

    bash/setup_terminal: add GC optimizations

--- bash/setup_terminal.sh
+++ bash/setup_terminal.sh
@@ -27,6 +27,13 @@ fi
 
 [ -f $DOTFILES_PATH/bash/bundler-exec.sh ] && source $DOTFILES_PATH/bash/bundler-exec.sh
 
+# optimize ruby garbage collector
+export RUBY_HEAP_MIN_SLOTS=1000000
+export RUBY_HEAP_SLOTS_INCREMENT=1000000
+export RUBY_HEAP_SLOTS_GROWTH_FACTOR=1
+export RUBY_GC_MALLOC_LIMIT=1000000000
+export RUBY_HEAP_FREE_MIN=500000
+
 export PATH="$PATH:$DOTFILES_PATH/vendor/tmuxinator/bin/"
 source $DOTFILES_PATH/vendor/tmuxinator/bin/tmuxinator_completion
 
