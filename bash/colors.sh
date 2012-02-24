68ffb3a86c162b2ce8adf71a87903e2d0cd47c85
Wojtek Majewski <jumski@gmail.com>
2012-02-24 02:25:47 +0100

    bash/colors: more fancy prompt

--- bash/colors.sh
+++ bash/colors.sh
@@ -7,31 +7,49 @@ c_violet="\[\e[34m\]"
 c_red="\[\e[31m\]"
 c_reset="\[\e[0m\]"
 
-function parse_git_dirty {
-  [[ $(git status 2> /dev/null | tail -n1) != "nothing to commit (working directory clean)" ]] && echo "*"
+function is_git_dirty {
+  (git status | tail -n1 | grep -v "nothing to commit") 2> /dev/null >/dev/null
 }
-function parse_git_branch {
-  git branch --no-color 2> /dev/null | sed -e '/^[^*]/d' -e "s/* \(.*\)/\1/"
+function is_git_repo {
+  (git status | grep -v "Not a git repository") 2>/dev/null >/dev/null
+}
+function current_branch {
+  echo $(git status 2>/dev/null | head -1 | awk {'print $4'})
+}
+function git_dirty_indicator {
+  is_git_dirty && echo '*'
+}
+function git_indicator {
+  if ! is_git_repo;
+  then
+    echo ''
+    return
+  fi
+
+  echo "${c_yellow}[$(current_branch)${c_red}$(git_dirty_indicator)$(is_git_dirty)${c_yellow}]$c_reset "
 }
 function parse_rvm_prompt {
   rvm-prompt | sed 's/\(ruby-\)\(.*\)@\(.*\)/\3/'
 }
-function parse_rails_env {
-  [ "$RAILS_ENV" = "production" ] && echo "${c_red}P$c_reset"
-  # [ "$RAILS_ENV" = "production" ] && echo "$c_red(P) $c_reset"
+function rails_env_indicator {
+  [ "$RAILS_ENV" = "production" ] && echo "${c_red}P$c_reset "
 }
 
-function get_color_hostname {
+function hostname_indicator {
   case `hostname` in
     jumski-laptop)
-      echo "${c_celadon}laptop$c_reset" ;;
+      echo "${c_celadon}laptop$c_reset " ;;
     jumski-old)
-      echo "${c_violet}old$c_reset" ;;
+      echo "${c_violet}old$c_reset " ;;
     jumski-akra)
-      echo "${c_green}akra$c_reset" ;;
+      echo "${c_green}akra$c_reset " ;;
     s11.linuxpl.com)
-      echo "${c_yellow}linuxpl$c_reset" ;;
+      echo "${c_yellow}linuxpl$c_reset " ;;
   esac
+}
+
+function pwd_indicator {
+  echo "${c_white}`basename "$PWD"`$c_reset "
 }
 
 function prompt_indicator {
@@ -42,7 +60,7 @@ function prompt_indicator {
     indicator_color=$c_red
   fi
 
-  echo "${indicator_color}$"
+  echo "${indicator_color}\$${c_reset} "
 }
 
-PROMPT_COMMAND='LAST_EXIT_CODE=$?;export PS1=" `get_color_hostname` $c_white`basename "$PWD"`$c_yellow [$(parse_git_branch)]`parse_rails_env` $c_red`prompt_indicator`$c_reset "'
+PROMPT_COMMAND='LAST_EXIT_CODE=$?;export PS1=" $(hostname_indicator)$(pwd_indicator)$(git_indicator)$(rails_env_indicator)$(prompt_indicator)"'
