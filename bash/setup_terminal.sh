e85a587d5e63279de2329437e12c5b80f6feba29
Wojtek Majewski <jumski@gmail.com>
2012-05-11 08:12:05 +0200

    startup: fixes

--- bash/setup_terminal.sh
+++ bash/setup_terminal.sh
@@ -41,7 +41,8 @@ source $DOTFILES_PATH/vendor/tmuxinator/bin/tmuxinator_completion
 bind "set completion-ignore-case on"
 bind "set show-all-if-ambiguous On" # this allows you to automatically show completion without double tab-ing
 
-. $HOME/startup/*
+# startup terminal things
+$HOME/startup/*
 
 # add identity if not set
 if ! ssh-add -l 1>/dev/null;
