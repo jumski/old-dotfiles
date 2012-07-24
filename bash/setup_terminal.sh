86c28e496754309290733561e87ee56fdf4f68bd
Wojtek Majewski <jumski@gmail.com>
2012-07-24 11:24:11 +0200

    keychain

--- bash/setup_terminal.sh
+++ bash/setup_terminal.sh
@@ -41,6 +41,9 @@ bind "set show-all-if-ambiguous On" # this allows you to automatically show comp
 # startup per-terminal thingzzz things
 $DOTFILES_PATH/startup/*
 
+# enable keychain
+eval `keychain --eval --agents ssh --inherit any`
+
 # add identity if not set
 if ! ssh-add -l 1>/dev/null;
 then
