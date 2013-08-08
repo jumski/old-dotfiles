244d23e351a0cd2eea3f1c6b760e8fa26e9be7f8
Wojtek Majewski <jumski@gmail.com>
2013-08-08 22:49:53 +0200

    lot of soma.fm radio-aliases, some logstash aliases

--- bash/aliases.sh
+++ bash/aliases.sh
@@ -43,16 +43,21 @@ alias jungletrain="mocp -l http://stream2.jungletrain.net:8000"
 alias bassdrive="mocp -l http://shouthost.com.32-1.streams.bassdrive.com:8200"
 alias livednb="mocp -l http://173.236.56.82:8006"
 alias iwayhigh="mocp -l http://70.38.71.74:8000"
-alias groovesalad="mocp -l http://mp3.somafm.com:80"
-alias sonicuniverse="mocp -l http://sfstream1.somafm.com:8600"
-alias suburbsofgoa="mocp -l http://sfstream1.somafm.com:8850"
-alias beatblender="mocp -l http://sfstream1.somafm.com:8384"
-alias dubstepbeyond="mocp -l http://mp4.somafm.com:8400"
-alias missioncontrol="mocp -l http://mp1.somafm.com:2020"
-alias streetlounge="mocp -l http://sfstream1.somafm.com:8500"
-alias dronezone="mocp -l http://mp4.somafm.com:80"
-alias spacestationsoma="mocp -l http://mp2.somafm.com:2666"
-alias ambientdrones="mocp -l http://srv3.electro-music.com:8500/"
+
+alias soma_groovesalad="mocp -l http://mp3.somafm.com:80"
+alias soma_sonicuniverse="mocp -l http://sfstream1.somafm.com:8600"
+alias soma_suburbsofgoa="mocp -l http://sfstream1.somafm.com:8850"
+alias soma_beatblender="mocp -l http://sfstream1.somafm.com:8384"
+alias soma_thetrip="mocp -l http://sfstream1.somafm.com:2504"
+alias soma_cliqhop="mocp -l http://mp2.somafm.com:2668"
+alias soma_doomed="mocp -l http://sfstream1.somafm.com:8300"
+alias soma_somaevents="mocp -l http://sfstream1.somafm.com:6200"
+alias soma_dubstepbeyond="mocp -l http://mp4.somafm.com:8400"
+alias soma_missioncontrol="mocp -l http://mp1.somafm.com:2020"
+alias soma_streetlounge="mocp -l http://sfstream1.somafm.com:8500"
+alias soma_dronezone="mocp -l http://mp4.somafm.com:80"
+alias soma_spacestationsoma="mocp -l http://mp2.somafm.com:2666"
+alias soma_ambientdrones="mocp -l http://srv3.electro-music.com:8500/"
 
 alias pg_start="pg_ctl -D /usr/local/var/postgres -l /usr/local/var/postgres/server.log start"
 alias pg_stop="pg_ctl -D /usr/local/var/postgres stop -s -m fast"
@@ -74,3 +79,15 @@ alias goprod="export RAILS_ENV=production; echo 'Rails.env set to production'"
 alias godev="export RAILS_ENV=development; echo 'Rails.env set to development'"
 
 alias joodo="lein joodo"
+
+
+
+
+
+## logstash specific
+disconf() {
+  mv $1 $(basename --suffix=.conf $1)
+}
+enconf() {
+  mv $1 ${1}.conf
+}
