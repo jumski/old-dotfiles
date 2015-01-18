5a966da69e91c44792611e18b73abcc7db081f22
Wojtek Majewski <jumski@gmail.com>
2015-01-18 19:51:46 +0100

    G and git aliased to hub

--- bash/aliases.sh
+++ bash/aliases.sh
@@ -18,7 +18,8 @@ alias lh='ls --color=always -lasth | less -R'
 alias alert='notify-send --urgency=low -i "$([ $? = 0 ] && echo terminal || echo error)" "$(history|tail -n1|sed -e '\''s/^\s*[0-9]\+\s*//;s/[;&|]\s*alert$//'\'')"'
 alias mkdir='mkdir -p'
 
-alias g=git
+alias g=hub
+alias git=hub
 alias gs="git status -sb"
 alias gps="git push"
 alias gd="git diff"
