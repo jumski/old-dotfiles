9111f7f00fa6bc95095a2d1718a2dfcd49d8b001
Wojtek Majewski <jumski@gmail.com>
2015-05-20 12:11:07 +0200

    Ankle remainder

--- bash/colors.sh
+++ bash/colors.sh
@@ -94,17 +94,19 @@ function prompt_indicator {
   echo "${indicator_color}\$${c_reset} "
 }
 
-function todo_oneliner {
-  # c_tip="\[\033[40m\]"
-  # oneliner=$(egrep -v "(^\"|^\s*$)" ~/dotfiles/todo.txt | shuf -n 1)
-  # echo "\n${c_tip}TODO: ${oneliner}${c_reset}\n"
-  echo ""
+function reminder {
+  c_tip="\[\033[40m\]"
+
+  if date +'%M' | egrep "(00|05|10|15|20|25|30|35|40|45|50|55)" 2>&1 >/dev/null; then
+    echo "\n      ${c_tip}!!!!!!!!!! ŁOKIEĆ !!!!!!!!!!!\n${c_reset}"
+    echo ""
+  fi
 }
 
 prompt_command() {
   LAST_EXIT_CODE=$?
   history -a
-  export PS1="$(todo_oneliner) $(hostname_indicator)$(pwd_indicator)$(git_indicator)$(rails_env_indicator)$(prompt_indicator)"
+  export PS1="$(reminder) $(hostname_indicator)$(pwd_indicator)$(git_indicator)$(rails_env_indicator)$(prompt_indicator)"
 }
 
 PROMPT_COMMAND=prompt_command
