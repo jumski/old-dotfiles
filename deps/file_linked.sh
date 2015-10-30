c3e325518c0e7a449b0b8dc40ed868d72563ecec
Wojtek Majewski <jumski@gmail.com>
2015-10-30 10:39:11 +0100

    Force file linking in deps/file_linked.sh

--- deps/file_linked.sh
+++ deps/file_linked.sh
@@ -5,13 +5,13 @@ file_linked() {
   is_met() {
     abs_src=`readlink -f $src`
 
-    test -h $dst && diff $abs_src $dst
+    (test -h $dst && diff $abs_src $dst) || (sudo test -h $dst && sudo diff $abs_src $dst)
   }
 
   meet() {
     abs_src=`readlink -f $src`
 
     log Linking \'$abs_src\' to \'$dst\'
-    ln -sf $abs_src $dst || sudo ln -s $abs_src $dst
+    ln -sf $abs_src $dst || sudo ln -sf $abs_src $dst
   }
 }
