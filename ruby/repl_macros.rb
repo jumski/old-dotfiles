6578aa50e2c0254051b4f7f6b953f6743fc8cf7b
Wojtek Majewski <jumski@gmail.com>
2017-03-01 13:50:29 +0100

    repl_macros

--- ruby/repl_macros.rb
+++ ruby/repl_macros.rb
@@ -25,18 +25,3 @@ end
 def _arr(num = 3)
     ('a'..'z').to_a[0, num]
 end
-
-def conjune(hours, usdpln = 3.95)
-  raise 'calm your tits' unless $calm_tits == true
-  hours * 30 * usdpln * 0.81
-end
-
-def monk(hours, eurpln = 4.05)
-  raise 'calm your tits' unless $calm_tits == true
-  hours * 20 * eurpln * 0.81
-end
-
-def rebased(hours, plnhr = 110)
-  raise 'calm your tits' unless $calm_tits == true
-  hours * plnhr * 0.81
-end
